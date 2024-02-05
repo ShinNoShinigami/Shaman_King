9732b633665a27efedc8b58a9b7d4233d23275dd
ShinNoShinigami <twood2486@gmail.com>
2024-02-05 15:35:16 +0000

    Souls Rei values

--- src/main/java/com/shin/shaman_king/AASAProcedure.java
+++ src/main/java/com/shin/shaman_king/AASAProcedure.java
@@ -1,47 +1,58 @@
 package com.shin.shaman_king;
 
 import com.shin.shaman_king.network.ShamanKingVariables;
-import net.minecraft.commands.CommandSource;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.monster.Blaze;
-import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.phys.Vec2;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.event.level.BlockEvent;
-import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import javax.annotation.Nullable;
 import java.util.Random;
 
 @Mod.EventBusSubscriber
 public class AASAProcedure {
 	@SubscribeEvent
 	public static void onBlockBreak(BlockEvent.BreakEvent event) {
-		if ((event.getLevel().getBlockState(BlockPos.containing(event.getPos().getX(), event.getPos().getY(), event.getPos().getZ()))).getBlock() == Blocks.POLISHED_DIORITE_SLAB) {
-
-		}
-		execute(event, event.getLevel(), event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getPlayer());
-	}
-	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
-		{
-			entity.getCapability(ShamanKingVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-				capability.Furyoku = 5;
-				capability.syncPlayerVariables(entity);
-			});
-		}
+		event.getPlayer().getCapability(ShamanKingVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+			capability.Furyoku = 5;
+			capability.syncPlayerVariables(event.getPlayer());
+		});
 	}
 	@SubscribeEvent
 	public static void onEntityDeath(LivingDeathEvent event){
-		if (event.getEntity() instanceof Blaze){
-			((Blaze) event.getEntity()).captureDrops();
-
+		ItemEntity entityToSpawn = new ItemEntity(event.getEntity().level(), event.getEntity().getX(), event.getEntity().getY(), event.getEntity().getZ(), new ItemStack(Blocks.DIRT));
+		event.getEntity().level().addFreshEntity(entityToSpawn);
+	}
+	@SubscribeEvent
+	public static void onSoulPickup(EntityItemPickupEvent event){
+		ItemStack itemstack = event.getItem().getItem();
+		if (!itemstack.hasTag()) {
+			int rangeMin = 0;
+			int rangeMax = 101;
+			Random r = new Random();
+			int SpiritType = r.nextInt(rangeMax - rangeMin) + rangeMin;
+			if(SpiritType == 100){
+				int ReiVal = r.nextInt(1000000)+100000;
+				itemstack.getOrCreateTag().putString("Spirit Type", "Kami");
+				itemstack.getOrCreateTag().putDouble("Reiryoku", 50);
+			}
+			else if (SpiritType >= 90){
+				itemstack.getOrCreateTag().putString("Spirit Type", "Seireitei");
+				int ReiVal = r.nextInt(100) < 1?
+						r.nextInt(50001)+50000:
+						r.nextInt(100) < 10 ?
+						r.nextInt(40001)+10000:
+						r.nextInt(9001)+1000;
+				itemstack.getOrCreateTag().putDouble("Reiryoku", ReiVal);
+			}
+			else{
+				int ReiVal = r.nextInt(1001);
+				itemstack.getOrCreateTag().putString("Spirit Type", "Seibutsu");
+				itemstack.getOrCreateTag().putDouble("Reiryoku", ReiVal);
+			}
 		}
 	}
 }
